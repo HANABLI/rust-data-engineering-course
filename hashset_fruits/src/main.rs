f98ad75b9797f25f9c94347e415b710920da587e
Hatem Nabli <34321336+HANABLI@users.noreply.github.com>
2025-06-08 21:32:50 +0000

    this is how to store unique fruits using hashset

---
+++ hashset_fruits/src/main.rs
@@ -0,0 +1,28 @@
+use rand::seq::SliceRandom;
+use rand::prelude::IndexedRandom;
+use rand::rng;
+use std::collections::HashSet;
+
+fn generate_fruits() -> &'static str {
+    let fruits = [
+        "Apple",
+        "Pear",
+        "Cherry",
+        "Fig",
+        "Date",
+        "Grape",
+        "Strawberry",
+        "Banana",
+    ];
+    let mut rng = rng();
+    fruits.choose(&mut rng).unwrap()
+}
+
+fn main() {
+    let mut fruit_set = HashSet::new();
+    println!("Generating 100 fruits.... ");
+    for _ in 0..100 {
+        fruit_set.insert(generate_fruits());
+    }
+    println!("Number of unique fruits generated: {}", fruit_set.len());
+}
